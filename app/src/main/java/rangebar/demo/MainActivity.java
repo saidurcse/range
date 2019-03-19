b0ab677ac0eea01e145d8532a752f0ea0d25ca63
saidurcse <saidursd@gmail.com>
2019-03-19 21:36:06 +0600

    First commit.

---
+++ app/src/main/java/rangebar/demo/MainActivity.java
@@ -0,0 +1,46 @@
+package rangebar.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
+import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarFinalValueListener;
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get seekbar from view
+        final CrystalRangeSeekbar rangeSeekbar = findViewById(R.id.rangeSeekbar1);
+
+        // get min and max text view
+        final TextView tvMin = findViewById(R.id.textMin1);
+        final TextView tvMax = findViewById(R.id.textMax1);
+
+        // set listener
+        rangeSeekbar.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
+            @Override
+            public void valueChanged(Number minValue, Number maxValue) {
+                tvMin.setText(String.valueOf(minValue));
+                tvMax.setText(String.valueOf(maxValue));
+            }
+        });
+
+        // set final value listener
+        rangeSeekbar.setOnRangeSeekbarFinalValueListener(new OnRangeSeekbarFinalValueListener() {
+            @Override
+            public void finalValue(Number minValue, Number maxValue) {
+                Log.d("CRS=>", String.valueOf(minValue) + " : " + String.valueOf(maxValue));
+                Toast.makeText(MainActivity.this, String.valueOf(minValue) + " : " + String.valueOf(maxValue), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
